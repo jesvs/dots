ff6cc7d5e3b73812ff77cc205f1e363cc7b0ede6
Jesus Sanchez <ahinosvemos@gmail.com>
2017-10-01 05:57:36 -0600

    new aliases, update vimrc

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -10,6 +10,17 @@ alias gs="git status"
 alias co="checkout"
 alias cob="checkout -b"
 
+alias be="bundle exec"
+alias ber="bundle exec rails"
+alias bers="bundle exec rails server"
+alias beg="bundle exec guard"
+alias bep="bundle exec pakyow"
+alias bepn="bundle exec pakyow new"
+alias beps="bundle exec pakyow server"
+alias code="visual-studio-code"
+
+alias vsc="visual-studio-code"
+
 function mkc {
     mkdir -p $1
     cd $1
